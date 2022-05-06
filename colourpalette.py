92d6a61f9e33ed907cbb65ed6c2024d25bcae2e5
dydxdata <dydxdata@pm.me>
2022-05-06 10:46:42 +0100

    create main file

---
+++ colourpalette.py
@@ -0,0 +1,12 @@
+'''
+Purpose: Create colour palettes from uploaded images
+CreatedBy: DBrookes
+CreatedOn: 06052022
+Notes:
+'''
+#requirements
+import streamlit as st
+import pandas as pd
+import numpy as np
+import PIL
+
